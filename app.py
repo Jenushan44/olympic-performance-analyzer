00ef3383267818de7dced800fe8aa845be018065
Jenushan <jenushan349@gmail.com>
2026-04-28 21:11:36 -0400

    Load processed Olympic data

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import streamlit as st 
 import pandas as pd 
 
+olympics_df = pd.read_csv("data/processed/olympics_cleaned.csv")
+top_country_medals_df = pd.read_csv("data/processed/top_country_medals.csv")
+top_country_medal_points_df = pd.read_csv("data/processed/top_country_medal_points.csv")
+medal_trends_df = pd.read_csv("data/processed/medal_trends.csv")
+country_top_sport_df = pd.read_csv("data/processed/country_top_sport.csv")
+specialized_countries_df = pd.read_csv("data/processed/specialized_countries.csv")
+
 st.set_page_config(
   page_title = "Olympic Performance Analyzer", 
   layout = "wide",
